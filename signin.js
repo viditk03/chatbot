c495fd35e7268acd26591d7c28bfa96ea6a74a48
Samyakmanwatkar10 <samyakmanwatkar@gmail.com>
2024-04-18 02:14:26 +0530

    Sign in & Login in page

--- signin.js
+++ signin.js
@@ -163,19 +163,3 @@ const firebaseConfig = {
     }
   }
   
-  const link1 = document.getElementById("link1");
-  const link2 = document.getElementById("link2");
-  const link3 = document.getElementById("link3");
-  
-  link1.addEventListener('click', () => {
-    scrollToElement('.header');
-  });
-  
-  link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.card');
-  });
-  
-  link3.addEventListener('click', () => {
-    scrollToElement('.containe');
-  });
